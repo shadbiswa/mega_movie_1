221796f5c26becd786422a7f05468861cc25deaa
Shadikshya Biswa <101527765+shadbiswa@users.noreply.github.com>
2022-03-23 21:13:50 +0000

    00 mmf base v1

---
+++ 00_mmf_base_v1.py
@@ -0,0 +1,29 @@
+#import statements 
+
+#funtions go here 
+
+
+
+#************* main routine ******
+
+#set up dictionaries / lists needed to hold data 
+
+#ask user if they used the program before & show instructions if necessary 
+
+#loop to get ticket deatils 
+
+  #get name (cant be blank)
+
+  #get age (between 12 and 130)
+
+  #calculate ticket price 
+
+  #loop to ask for snacks
+
+  #calculate snack price
+
+  # ask for payment method (and apply surcharge if necesary)
+
+#calculate total sales and profit
+
+#output data to text file 
